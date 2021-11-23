f466f83a86dc858a206a6f5c16e68a60e241e024
gabrielaabergamo <gabiibergamo@gmail.com>
2021-11-23 10:20:47 -0300

    programa feito

--- topdf/topdf.go
+++ topdf/topdf.go
@@ -1,8 +1,29 @@
 package main
 
-// import(
-// 	"fmt"
-// 	"github.com/jung-kurt/gofpdf"
-// 	"io/ioutil"
-// 	"log"
-// )
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+	file := "text.txt"
+
+	content, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatalf("%s file not found", file)
+	}
+
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 15)
+
+	pdf.MultiCell(190, 5, string(content), "0", "0", false)
+
+	_ = pdf.OutputFileAndClose("test.pdf")
+
+	fmt.Println("PDF created")
+}
